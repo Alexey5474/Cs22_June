fcec8acd880417e0392491b230bb6255e756e49c
Alexey5474 <G.A.V.ik@mail.ru>
2022-06-17 18:55:57 +0600

    Практическая 3(задача 12)

---
+++ С№/Reshenie/Test_Git/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите делимое:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите делитель:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int res1 = a / b;
+            int res2 = a % b;
+            Console.WriteLine($"Делимое {a}, делитель {b}, частное {res1}, остаток {res2}");
+            Console.ReadKey();
+
+        }
+    }
+}
